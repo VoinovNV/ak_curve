914b91f50c67dc8cdb12a3c869dc011ce091cf9f
VoinovNV <nvvoinov@edu.hse.ru>
2020-08-08 17:03:11 +0300

    init

--- build/make-uninstall-0.7.15.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-rm -fv /usr/local/lib/libakrypt-static.a
-rm -fv /usr/local/lib/libakrypt-shared.*
-rm -fv /usr/local/include/libakrypt.h
-rm -fv /usr/local/include/ak_*
-rm -fv /etc/libakrypt.conf
